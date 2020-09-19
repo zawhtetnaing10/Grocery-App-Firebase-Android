566d09cc2ee4a1ec94ddfc65157eebdce691d5e3
zawhtetnaing10 <zawgyi.gog@gmail.com>
2020-09-19 10:38:08 +0630

    Cloud Firestore Add Data

--- app/src/main/java/com/padc/grocery/network/CloudFirestoreFirebaseApiImpl.kt
+++ app/src/main/java/com/padc/grocery/network/CloudFirestoreFirebaseApiImpl.kt
@@ -1,5 +1,6 @@
 package com.padc.grocery.network
 
+import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.padc.grocery.data.vos.GroceryVO
@@ -32,7 +33,17 @@ object CloudFirestoreFirebaseApiImpl : FirebaseApi {
     }
 
     override fun addGrocery(name: String, description: String, amount: Int) {
+        val groceryMap = hashMapOf(
+            "name" to name,
+            "description" to description,
+            "amount" to amount.toLong()
+        )
 
+        db.collection("groceries")
+            .document(name)
+            .set(groceryMap)
+            .addOnSuccessListener { Log.d("Success", "Successfully added grocery") }
+            .addOnFailureListener { Log.d("Failure", "Failed to add grocery") }
     }
 
     override fun deleteGrocery(name: String) {
