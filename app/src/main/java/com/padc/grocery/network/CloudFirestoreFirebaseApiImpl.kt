b4be5551a72114ff7d14ee92afc4e895d527e8f0
zawhtetnaing10 <zawgyi.gog@gmail.com>
2020-09-25 07:25:41 +0630

    Cloud Firestore image upload implemented

--- app/src/main/java/com/padc/grocery/network/CloudFirestoreFirebaseApiImpl.kt
+++ app/src/main/java/com/padc/grocery/network/CloudFirestoreFirebaseApiImpl.kt
@@ -4,11 +4,16 @@ import android.graphics.Bitmap
 import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.padc.grocery.data.vos.GroceryVO
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 object CloudFirestoreFirebaseApiImpl : FirebaseApi {
 
     val db = Firebase.firestore
+    private val storage = FirebaseStorage.getInstance()
+    private val storageReference = storage.reference
 
     override fun getGroceries(
         onSuccess: (groceries: List<GroceryVO>) -> Unit,
@@ -59,7 +64,29 @@ object CloudFirestoreFirebaseApiImpl : FirebaseApi {
     }
 
     override fun uploadImageAndEditGrocery(image: Bitmap, grocery: GroceryVO) {
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
 
+        val imageRef = storageReference.child("images/${UUID.randomUUID()}")
+        val uploadTask = imageRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            //
+        }.addOnSuccessListener { taskSnapshot ->
+            //
+        }
+
+        val urlTask = uploadTask.continueWithTask {
+            return@continueWithTask imageRef.downloadUrl
+        }.addOnCompleteListener { task ->
+            val imageUrl = task.result?.toString()
+            addGrocery(
+                grocery.name ?: "",
+                grocery.description ?: "",
+                grocery.amount ?: 0,
+                imageUrl ?: ""
+            )
+        }
     }
 
 }
