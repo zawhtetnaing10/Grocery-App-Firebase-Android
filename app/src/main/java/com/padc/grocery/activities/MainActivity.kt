7deea87b3499cf0aea2617c5913be6c43315539f
zawhtetnaing10 <zawgyi.gog@gmail.com>
2020-10-25 19:47:57 +0630

    Crashlytics added

--- app/src/main/java/com/padc/grocery/activities/MainActivity.kt
+++ app/src/main/java/com/padc/grocery/activities/MainActivity.kt
@@ -9,6 +9,8 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
@@ -49,6 +51,20 @@ class MainActivity : BaseActivity(), MainView {
         setUpActionListeners()
 
         mPresenter.onUiReady(this, this)
+
+        addCrashButton()
+    }
+
+    private fun addCrashButton(){
+        val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
